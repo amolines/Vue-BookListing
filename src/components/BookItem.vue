cb327e1642d7ba94ed0c738112735c4c14a606e4
Alejandro Molines <alemol32@gmail.com>
2019-09-29 00:41:03 +0200

    Creating a Child Component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,15 @@
+<template>{{book.title}}:{{book.author}}</template>
+
+<script>
+export default {
+  name: 'BookItem',
+  props: ['book']
+};
+</script>
+
+<style scoped>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
